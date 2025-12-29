74e69ecbd247684cdbdc9d2ae7f64ff27cad9cf3
a224327780 <a224327780@gmail.com>
2025-12-29 10:11:49 +0000

    init

---
+++ .idx/dev.nix
@@ -0,0 +1,75 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-24.05"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.go
+    pkgs.python311
+    # pkgs.python311Packages.pip
+    pkgs.nodejs_20
+    pkgs.openssh
+    # pkgs.docker
+    # pkgs.docker-compose
+    # pkgs.nodePackages.nodemon
+  ];
+  # Sets environment variables in the workspace
+  env = {
+  };
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      # "vscodevim.vim"
+      "google.gemini-cli-vscode-ide-companion"
+      "ms-python.python"
+      "ms-python.black-formatter"
+      "dracula-theme.theme-dracula"
+      "golang.go"
+      "pkief.material-icon-theme"
+      "esbenp.prettier-vscode"
+      "esbenp.foxundermoon.shell-format"
+    ];
+    # Enable previews
+    previews = {
+      enable = true;
+      previews = {
+        # web = {
+        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
+        #   # and show it in IDX's web preview panel
+        #   command = ["npm" "run" "dev"];
+        #   manager = "web";
+        #   env = {
+        #     # Environment variables to set for your server
+        #     PORT = "$PORT";
+        #   };
+        # };
+      };
+    };
+    # Workspace lifecycle hooks
+    workspace = {
+      # Runs when a workspace is first created
+      onCreate = {
+        # Example: install JS dependencies from NPM
+        # npm-install = "npm install";
+        # Open editors for the following files by default, if they exist:
+        python-venv = "python -m venv .venv";
+        default.openFiles = [ "README.md" ];
+      };
+      # Runs when the workspace is (re)started
+      onStart = {
+        # Example: start a background task to watch and re-build backend code
+        python-active = "source .venv/bin/activate";
+      };
+    };
+  };
+  services.docker = {
+    enable = true;
+  };
+  services.mongodb = {
+    enable = true;
+  };
+  services.redis = {
+    enable = true;
+  };
+}
