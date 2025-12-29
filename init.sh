f16aa4721c9d6b5d4d41100d02dc63d045d69081
riverecho520 <riverecho520@gmail.com>
2025-12-29 11:48:41 +0000

    oncreate

---
+++ init.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+mkdir ~/bin
+curl -fsSL https://deno.land/install.sh | bash
+export PATH="~/.deno/bin:$PATH"
+deno install -gArf jsr:@deno/deployctl
+
+wget https://github.com/fastfetch-cli/fastfetch/releases/download/2.56.1/fastfetch-linux-amd64.zip
+unzip fastfetch-linux-amd64.zip
+mv fastfetch-linux-amd64/usr/bin/* ~/bin/
+rm -rf fastfetch-linux-amd64*
+
+npm install esa-cli -g
+npm install edgeone -g
+npm install wrangler -g
+npm install @anthropic-ai/claude-code -g
+
+python -m venv .venv
+
+echo 'export PATH=~/bin:$PATH' >>~/.zshrc
