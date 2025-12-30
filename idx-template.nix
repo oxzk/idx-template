195465bd954994facc1c90ee705bd1d740eb11bb
riverecho520 <riverecho520@gmail.com>
2025-12-30 06:09:55 +0000

    tempalte

--- idx-template.nix
+++ idx-template.nix
@@ -1,18 +1,12 @@
 { pkgs, ... }: {
   packages = [
-    pkgs.nodejs_20
-    pkgs.python311
   ];
   bootstrap = ''   
-    mkdir "$out"
-    mkdir "$out"/.idx
-    mkdir "$out"/.vscode
-    mkdir "$out"/.bin
+    mkdir -p "$out"/.idx "$out"/.vscode "$out"/.bin
     cp -r ${./.idx}/. "$out/.idx/"
     cp -r ${./.vscode}/. "$out/.vscode"
     cp -r ${./.bin}/. "$out/.bin"
-    echo 'export PATH="$HOME/$out/.bin:$PATH"' >> ~/.bashrc
+    chmod +x "$out/.bin/"*
     chmod -R u+w "$out"
-    rm -rf "$out/.git"
   '';
 }
