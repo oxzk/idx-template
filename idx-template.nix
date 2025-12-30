00e1c7806498b2a610ec46cd655acab9e2b4ddc9
riverecho520 <riverecho520@gmail.com>
2025-12-30 03:59:10 +0000

    tempalte

--- idx-template.nix
+++ idx-template.nix
@@ -9,14 +9,6 @@
     cp -r ${./.}/* "$WS_NAME"
     chmod -R +w "$WS_NAME"
     mv "$WS_NAME" "$out"
-
-    mkdir -p "$out/.idx"
-    cp -rf ${./.idx/dev.nix} "$out/.idx/dev.nix"
-    chmod -R u+w "$out"
-
     cd "$out"
-    npm install wrangler -g
-    npm install @anthropic-ai/claude-code -g
-    python -m venv .venv
   '';
 }
