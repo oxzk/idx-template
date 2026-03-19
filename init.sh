625afaad7a0c2ea1fe786dba762697c8094b4d3d
riverecho520 <riverecho520@gmail.com>
2026-03-19 21:11:38 +0800

    init

--- init.sh
+++ init.sh
@@ -12,6 +12,7 @@ curl -fsSL https://deno.land/install.sh | \
 export PATH="~/.deno/bin:$PATH"
 deno install -gArf jsr:@deno/deployctl
 
+mkdir -p "$HOME/.local/bin"
 wget https://github.com/fastfetch-cli/fastfetch/releases/download/2.56.1/fastfetch-linux-amd64.zip
 unzip fastfetch-linux-amd64.zip
 mv fastfetch-linux-amd64/usr/bin/* "$HOME/.local/bin"
@@ -19,8 +20,8 @@ rm -rf fastfetch-linux-amd64*
 
 # npm install esa-cli -g
 # npm install edgeone -g
-# npm install -g @openai/codex
 
+npm install -g @openai/codex
 npm install wrangler -g
 npm install @anthropic-ai/claude-code -g
 
@@ -55,7 +56,7 @@ cat > README.md <<'EOF'
 git checkout --orphan empty-branch
 ALTER USER "user" WITH PASSWORD 'admin001';
 
-curl https://kfc-api.sxxe.net/v1/chat/completions \
+curl https://welfare.apikey.cc/v1/chat/completions \
   -H "Content-Type: application/json" \
   -H "Authorization: Bearer xxx" \
   -d '{
